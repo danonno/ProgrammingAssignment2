ae43ac834880efe1764da022bc2082a985720e2c
David Nonno <davidnonno@mini.local>
2014-11-14 15:08:07 -0500

    Template

--- cachematrix.R
+++ cachematrix.R
@@ -1,12 +1,21 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
+## Write a short comment describing this function 
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  m <- NULL
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
 }
-
+  get <- function() x
+  setmean <- function(mean) m <<- mean
+  getmean <- function() m
+  list(set = set, get = get,
+     setmean = setmean,
+     getmean = getmean)
+}
 
 ## Write a short comment describing this function
 
